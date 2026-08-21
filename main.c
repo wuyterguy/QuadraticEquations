ab223dd1de9dfec3020253e72134b74110d6fc73
Zotov Anton <waytergay.02@gmail.com>
2026-08-21 19:41:45 +0300

    file input, color output

--- main.c
+++ main.c
@@ -2,40 +2,90 @@
 
 int main() {
     double coef_a = 0.0, coef_b = 0.0, coef_c = 0.0;
-    if (CoefficientsRequest(&coef_a, &coef_b, &coef_c)) {
-        return 0;
+
+    inptype askt_result = AskInputType();
+    if (askt_result == FILE_INPUT) {
+        if (FileRequestCoefficients(&coef_a, &coef_b, &coef_c))
+            return 1;
     }
+    else if (askt_result == CONSOLE_INPUT) {
+        int req_result = RequestCoefficients(&coef_a, &coef_b, &coef_c);
+        if (req_result == 1)
+            return 0;
+        else if (req_result == 2)
+            return 2;
+    }
+    else
+        return 3;
 
     double root1 = 0.0, root2 = 0.0;
-    rootsc nroots = QuadraticSolver(coef_a, coef_b, coef_c, &root1, &root2);
+    rootsc nroots = SolveQuadratic(coef_a, coef_b, coef_c, &root1, &root2);
 
     if (PrintRoots(nroots, root1, root2)) {
-        fprintf(stderr, "PrintRoots: Unexpected value of nroot\n");
-        return 2;
+        fprintf(stderr, Color("PrintRoots: Unexpected value of nroot\n", RED));
+        return 4;
     }
 
     return 0;
 }
 
 
-int CoefficientsRequest(double* const p_coef_a, double* const p_coef_b, double* const p_coef_c) {    //TODO any parameters count
+inptype AskInputType() {
+    printf("Quadratic equations solver by " Color("Zotov Anton", YEL) "\n");
+    printf("Would you like to use file input from file " Color("coefficients.txt", YEL) "? (write " Color("Y", MAG) " or " Color("N", MAG) ")\n");
+    printf("If you write N it will be console input\n");
+
+    answ ask_result = AskYesOrNo();
+    if (ask_result == YES)
+        return FILE_INPUT;
+    else if (ask_result == NO)
+        return CONSOLE_INPUT;
+    else
+        return ERR;
+}
+
+
+int FileRequestCoefficients(double* const p_coef_a, double* const p_coef_b, double* const p_coef_c) {
+    FILE* coefficients_file = fopen("coefficients.txt", "r");
+
+    if (coefficients_file == NULL) {
+        printf("file " Color("coefficients.txt", YEL) "doesn't exist or you don't have \"r\" permission\n");
+        return 1;
+    }
+
+    if (fscanf(coefficients_file, "%lf %lf %lf", p_coef_a, p_coef_b, p_coef_c) != 3) {
+        printf(Color("FileRequestCoefficients:", YEL) "There must be three coefficients like: " Color("1 2 1", MAG) "\n");
+        return 2;
+    }
+
+    fclose(coefficients_file);
+    return 0;
+}
+
+
+int RequestCoefficients(double* const p_coef_a, double* const p_coef_b, double* const p_coef_c) {    //TODO any parameters count
     assert(p_coef_a != NULL);
     assert(p_coef_b != NULL);
     assert(p_coef_c != NULL);
     assert(p_coef_a != p_coef_b && p_coef_a != p_coef_c && p_coef_b != p_coef_c);
 
-    printf("Quadratic equations solver by Zotov Anton\n");
     while(1) {                          // request the introduction of coefficients unless input is correct or program is closed
-        printf("Enter the coefficients coef_a, coef_b, coef_c like: 1 2 1\n");
+        printf("Enter the coefficients coef_a, coef_b, coef_c like: " Color("1 2 1", MAG) "\n");
 
-        if (scanf("%lf %lf %lf", p_coef_a, p_coef_b, p_coef_c) != 3){
-            printf("CoefficientsRequest: There must be three coefficients\n");
-            printf("Do you want to try again? (write Y if you do or N otherwise)\n");
+        if (scanf("%lf %lf %lf", p_coef_a, p_coef_b, p_coef_c) != 3) {
+            ClearInputBuf();        // clear the input buffer from excess symbols
 
-            if(AskYesOrNo())            // answer is Y
+            printf(Color("RequestCoefficients:", YEL) "There must be three coefficients like: " Color("1 2 1", MAG) "\n");
+            printf("Do you want to try again? (write " Color("Y", MAG) " if you do or " Color("N", MAG) " otherwise)\n");
+
+            answ ask_result = AskYesOrNo();
+            if (ask_result == YES)
                 continue;
-            else                        // answer is N
+            else if (ask_result == NO)
                 return 1;
+            else
+                return 2;
+
         }
         else /* number of arguments is 3 => input is correct */
             break;
@@ -45,16 +95,29 @@ int CoefficientsRequest(double* const p_coef_a, double* const p_coef_b, double* 
 }
 
 
-int AskYesOrNo() {
-    char answer = 0;
-    while(1) {                  // request answer unless it is correct
-        ClearInputBuf();        // clear the input buffer from excess symbols
-        scanf("%c", &answer);
-        if (answer == 'Y')
-            return 1;
-        else if (answer == 'N')
-            return 0;
-        printf("PLease write Y or N\n");
+answ AskYesOrNo() {
+    char c;
+    answ result = NONE;
+    while(1) {                      // request answer unless it is correct
+        result = NONE;
+        bool clear = true;
+        c = getchar();
+        if (c == 'Y')
+            result = YES;
+        else if (c  == 'N')
+            result = NO;
+        else if ((c == '\n') || (c == EOF))
+            clear = false;          // buffer is empty
+        if (result != NONE) {
+            c = getchar();
+            if((c == '\n') || (c == EOF))
+                return result;
+        }
+
+        if (clear)
+            ClearInputBuf();
+
+        printf("PLease write " Color("Y", MAG) " or " Color("N", MAG) "\n");
     }
 }
 
@@ -65,58 +128,61 @@ void ClearInputBuf(){
 }
 
 
-rootsc QuadraticSolver(const double coef_a, const double coef_b, const double coef_c, double* const p_root1, double* const p_root2) {
+rootsc SolveQuadratic(const double coef_a, const double coef_b, const double coef_c,
+                       double* const p_root1, double* const p_root2) {
     assert(p_root1 != NULL);
     assert(p_root2 != NULL);
     assert(p_root1 != p_root2);
 
-    if (EpsPrecComparison(coef_a, 0)) {
-        return LinearSolver(coef_b, coef_c, p_root1);
+    if (CmpEpsPrec(coef_a, 0)) {
+        return SolveLinear(coef_b, coef_c, p_root1);
     }
     else { /* (coef_a != 0) => it's quadric education */
-        double D = coef_b*coef_b - 4*coef_a*coef_c;
-        if (EpsPrecComparison(D, 0)) {
-            *p_root1 = SmartDivision(-coef_b, 2 * coef_a);
+        double discr = coef_b*coef_b - 4*coef_a*coef_c;
+        if (CmpEpsPrec(discr, 0)) {
+            *p_root1 = DivideSmart(-coef_b, 2*coef_a);
             return ONEROOT;
         }
-        else if (D > 0) {
-            double sqD = sqrt(D);
-            *p_root1 = SmartDivision(-coef_b + sqD, 2*coef_a);
-            *p_root2 = SmartDivision(-coef_b - sqD, 2*coef_a);
+        else if (discr > 0) {
+            double sq_discr = sqrt(discr);
+            *p_root1 = DivideSmart(-coef_b + sq_discr, 2*coef_a);
+            *p_root2 = DivideSmart(-coef_b - sq_discr, 2*coef_a);
             return TWOROOTS;
         }
-        else { /* (D < 0) => there is no solutions */
+        else { /* (discr < 0) => there is no solutions */
             return NOROOTS;
         }
     }
 }
 
 
-rootsc LinearSolver(const double coef_a, const double coef_b, double* const p_root) {
+rootsc SolveLinear(const double coef_a, const double coef_b, double* const p_root) {
     assert(p_root != NULL);
 
-    if (EpsPrecComparison(coef_a, 0)) {
-        if (EpsPrecComparison(coef_b, 0))
+    if (CmpEpsPrec(coef_a, 0)) {
+        if (CmpEpsPrec(coef_b, 0))
             return INFROOTS;
         else /* (coef_c != 0) */
             return NOROOTS;
     }
     else { /* (coef_b != 0) */
-        *p_root = SmartDivision(-coef_b, coef_a);
+        *p_root = DivideSmart(-coef_b, coef_a);
         return ONEROOT;
     }
 }
 
 
-double SmartDivision(const double dividend, const double divider) {
-    if (!EpsPrecComparison(dividend, 0))
+double DivideSmart(const double dividend, const double divider) {
+    assert(divider != 0);
+
+    if (!CmpEpsPrec(dividend, 0))
         return dividend / divider;
     else
         return 0;
 }
 
 
-int EpsPrecComparison(const double a, const double b) {
+bool CmpEpsPrec(const double a, const double b) {
     if ((a <= b + EPSILON) && (a >= b - EPSILON))
         return 1;
     else
@@ -125,7 +191,7 @@ int EpsPrecComparison(const double a, const double b) {
 
 
 int PrintRoots(rootsc nroots, const double root1, const double root2) {
-    if(EpsPrecComparison(nroots, TWOROOTS) && EpsPrecComparison(root1, root2))
+    if(CmpEpsPrec(nroots, TWOROOTS) && CmpEpsPrec(root1, root2))
         nroots = ONEROOT;
 
     switch(nroots) {
@@ -136,10 +202,10 @@ int PrintRoots(rootsc nroots, const double root1, const double root2) {
             printf("There isn't any solution\n");
             break;
         case ONEROOT:
-            printf("There is only solution x1: %.5lf\n", root1);
+            printf("There is only solution x1: " Color("%.5lf", MAG) "\n", root1);
             break;
         case TWOROOTS:
-            printf("There are two solutions x1: %.5lf; x2: %.5lf\n", root1, root2);
+            printf("There are two solutions x1: " Color("%.5lf", MAG) "; x2: " Color("%.5lf", MAG) "\n", root1, root2);
             break;
         default:
             return 1;
--- title.h
+++ title.h
@@ -6,16 +6,45 @@
 
 #define EPSILON 0.00001                     //comparison accuracy
 typedef enum {NOROOTS, ONEROOT, TWOROOTS, INFROOTS = -2} rootsc;
+typedef enum {FILE_INPUT, CONSOLE_INPUT, ERR} inptype;
+typedef enum {NO, YES, NONE} answ;
 
-/* CoefficientsRequest: transmit three values from input to addresses pcoef_a, pcoef_b, pcoef_c.
-Returns 1 if the user wants to close the program or 0 otherwise. */
-int CoefficientsRequest(double* const p_coef_a, double* const p_coef_b, double* const p_coef_c);
+#define ON  1
+#define OFF 0
 
-/* QuadraticSolver: finds the solution of quadratic equation with coefficients coef_a, coef_b, coef_c
+#define COLORSWITCH ON                  // switch color output
+
+#if COLORSWITCH == ON
+    #define RED "\x1b[31m"
+    #define BLU "\x1b[34m"
+    #define GRE "\x1b[32m"
+    #define YEL "\x1b[33m"
+    #define MAG "\x1b[35m"
+    #define END "\x1b[0m"
+
+    /* Color: is replaced by s colored in color */
+    #define Color(s, color) color s END
+#else
+    #define Color(s, color) s
+#endif
+
+/* AskInputType: asks the user to choose input type.
+Returns FILE_INPUT or CONSOLE_INPUT or ERR if there is error. */
+inptype AskInputType(void);
+
+/* FileRequestCoefficients: transmit three values from coefficients.txt to addresses pcoef_a, pcoef_b, pcoef_c.
+Returns 0 if everything is correct or 1 if there is file read error or 2 if there is fscanf error. */
+int FileRequestCoefficients(double* const p_coef_a, double* const p_coef_b, double* const p_coef_c);
+
+/* RequestCoefficients: transmit three values from input to addresses pcoef_a, pcoef_b, pcoef_c.
+Returns 1 if the user wants to close the program or 0 otherwise or 2 if there is error. */
+int RequestCoefficients(double* const p_coef_a, double* const p_coef_b, double* const p_coef_c);
+
+/* SolveQuadratic: finds the solution of quadratic equation with coefficients coef_a, coef_b, coef_c
 and writes them to the address proot1, proot2.
 If equation has only one solution, it will be written to addres proot1.
 Returns the number of solutions or INFROOTS if there are infinite. */
-rootsc QuadraticSolver(const double coef_a, double const coef_b, double const coef_c, double* const p_root1, double* const p_root2);
+rootsc SolveQuadratic(const double coef_a, const double coef_b, const double coef_c, double* const p_root1, double* const p_root2);
 
 /* PrintRoots: print roots depending on the value of nroots.
 Returns 1 if everything is correct or 0 otherwise. */
@@ -23,19 +52,20 @@ int PrintRoots(rootsc nroots, const double root1, const double root2);
 
 /* AskYesOrNo: asks the user to write Y or N unless he do it.
 Returns 1 if answer is Y or 0 if answer is N. */
-int AskYesOrNo(void);
+answ AskYesOrNo(void);
 
-/* ClearInputBuf: clears the input buffer from excess symbols */
+/* ClearInputBuf: clears the input buffer from excess symbols.
+Buffer mustn't be empty. */
 void ClearInputBuf(void);
 
-/* EpsPrecComparison: compares a and b taking EPSILON as accuracy into account.
+/* CmpEpsPrec: compares a and b taking EPSILON as accuracy into account.
 Returns 1 if a = b or 0 otherwise. */
-int EpsPrecComparison(const double a, const double b);
+bool CmpEpsPrec(const double a, const double b);
 
-/* LinearSolver: finds the solution of linear equation with coefficients coef_a, coef_b
+/* SolveLinear: finds the solution of linear equation with coefficients coef_a, coef_b
 and writes them to the address proot.
 Returns the number of solutions or INFROOTS if there are infinite. */
-rootsc LinearSolver(const double coef_a, const double coef_b, double* const p_root);
+rootsc SolveLinear(const double coef_a, const double coef_b, double* const p_root);
 
-/* SmartDivision: returns the quotient of dividend/divider preventing the return of -0.0 */
-double SmartDivision(const double dividend, const double divider);
+/* DivideSmart: returns the quotient of dividend/divider preventing the return of -0.0 */
+double DivideSmart(const double dividend, const double divider);
